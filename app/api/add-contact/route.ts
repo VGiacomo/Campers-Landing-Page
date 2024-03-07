887bb68d44ef965892dbb7b311e6594b3eb09a00
Giacomo <v.giacomo3@gmail.com>
2024-03-07 17:48:37 +0100

    POST add-contact without prisma

--- app/api/add-contact/route.ts
+++ app/api/add-contact/route.ts
@@ -1,34 +1,35 @@
 // app/api/add-contact/route.ts
-import { PrismaClient } from "@prisma/client";
+import { sql } from "@vercel/postgres";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
-export async function POST(request: Request) {
-  const { name, email } = await request.json();
+export async function POST(req: NextRequest) {
+  // Parse the JSON body
+  const body = await req.json();
+  const { contactName, contactEmail } = body;
 
   try {
-    // Create a new contact
-    const contact = await prisma.contact.create({
-      data: {
-        name,
-        email,
-      },
-    });
+    if (!contactName || !contactEmail) {
+      throw new Error("Name and Email required");
+    }
 
-    // Optionally, retrieve all contacts
-    // const contacts = await prisma.contact.findMany();
+    // Execute SQL query to insert the new contact
+    await sql`INSERT INTO Contacts (Name, Email) VALUES (${contactName}, ${contactEmail});`;
+    // Retrieve all contacts (consider optimizing this if the contacts list is large)
+    const contacts = await sql`SELECT * FROM Contacts;`;
 
-    return new NextResponse(JSON.stringify({ contact }), {
+    // Return the contacts in the response
+    return new NextResponse(JSON.stringify({ contacts }), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
       },
     });
-  } catch (error) {
-    console.error("Request error", error);
+  } catch (error: unknown) {
+    // Handle errors, such as invalid input or database errors
+    console.error(error);
     return new NextResponse(
-      JSON.stringify({ error: "Error creating contact" }),
+      JSON.stringify({ error: (error as Error).message }),
       {
         status: 500,
         headers: {
